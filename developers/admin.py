96e3cf6a582ed9beec831a62e5723af2d10793dc
PaulyEd <pauledwardswx@gmail.com>
2020-11-08 18:36:23 +0000

    added developer view and templates, also changed some pictures for smaller size for faster load times

--- developers/admin.py
+++ developers/admin.py
@@ -2,6 +2,41 @@ from django.contrib import admin
 from .models import Developer, Framework, Language
 
 # Register your models here.
-admin.site.register(Developer)
-admin.site.register(Framework)
-admin.site.register(Language)
+
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'get_framework',
+        'get_language',
+        'rate',
+        'rating',
+        'image',
+    )
+
+    def get_framework(self, obj):
+        return ", ".join([p.friendly_name for p in obj.framework.all()])
+        
+    def get_language(self, obj):
+        return ", ".join([p.friendly_name for p in obj.language.all()])
+
+    ordering = ('name',)
+
+
+class FrameworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Developer, DeveloperAdmin)
+admin.site.register(Framework, FrameworkAdmin)
+admin.site.register(Language, LanguageAdmin)
--- developers/views.py
+++ developers/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Developer
 
 # Create your views here.
+
+
+def all_developers(request):
+    """ Return the all the developers available on the app """
+
+    developers = Developer.objects.all()
+
+    context = {
+        'developers' : developers,
+    }
+
+    return render(request, 'developers/developers.html', context)
