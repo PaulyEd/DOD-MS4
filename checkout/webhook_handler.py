b8d6a6f25dfa3e2441639a6c5143db9fae019678
PaulyEd <pauledwardswx@gmail.com>
2021-01-06 14:56:38 +0000

    updated checkout webhook handler to save user profile

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from developers.models import Developer
+from profiles.models import UserProfile
 
 import json
 import time
@@ -23,7 +24,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
-        print("WEBHOOK ACTIVATED") #FOR DEBUGGING
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
@@ -32,11 +32,28 @@ class StripeWH_Handler:
         billing_details = intent.charges.data[0].billing_details
         grand_total = round(intent.charges.data[0].amount / 100, 2)
         print(billing_details)
+
         # Clean data in the shipping details
         for field, value in billing_details.address.items():
             if value == "":
                 billing_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_full_name = billing_details.name
+                profile.default_phone_number = billing_details.phone
+                profile.default_country = billing_details.address.country
+                profile.default_postcode = billing_details.address.postal_code
+                profile.default_town_or_city = billing_details.address.city
+                profile.default_street_address1 = billing_details.address.line1
+                profile.default_street_address2 = billing_details.address.line2
+                profile.default_county = billing_details.address.state
+                profile.save()
+        
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -60,9 +77,7 @@ class StripeWH_Handler:
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
-                print("ORDER NOT FOUND") #FOR DEBUGGING
         if order_exists:
-            print("ORDER FOUND") #FOR DEBUGGING
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -71,6 +86,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=billing_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=billing_details.phone,
                     country=billing_details.address.country,
