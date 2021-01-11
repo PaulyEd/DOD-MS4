670a7a8a573e8649ae046f4cf986a2e53240e203
PaulyEd <pauledwardswx@gmail.com>
2021-01-11 16:20:44 +0000

    updated add/edit templates image field, updated bag context to rest bag if item deleted whilst in bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,26 +4,35 @@ from developers.models import Developer
 
 
 def bag_contents(request):
+    """" Try/Except block to catch if a developer has been deleted whilst in user bag to prevent session 404"""
 
     bag_items = []
     total = 0
     bag_item_count = 0
     bag = request.session.get('bag', {})
+    try:
+        for item_id, quantity in bag.items():
+            developer = get_object_or_404(Developer, pk=item_id)
+            total += quantity * developer.rate
+            bag_item_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'developer': developer,
+            })
 
-    for item_id, quantity in bag.items():
-        developer = get_object_or_404(Developer, pk=item_id)
-        total += quantity * developer.rate
-        bag_item_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'developer': developer,
-        })
+        context = {
+            'bag_items': bag_items,
+            'total': total,
+            'bag_item_count': bag_item_count,
+        }
 
-    context = {
-        'bag_items': bag_items,
-        'total': total,
-        'bag_item_count': bag_item_count,
-    }
+    except:
+        del request.session['bag']
+        context = {
+            'bag_items': '',
+            'total': 0,
+            'bag_item_count': 0,
+        }
 
     return context
--- developers/forms.py
+++ developers/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Language, Spoken_Language, Framework, Developer
+from .widgets import CustomClearableFileInput
 
 
 class DeveloperForm(forms.ModelForm):
@@ -7,6 +8,8 @@ class DeveloperForm(forms.ModelForm):
     class Meta:
         model = Developer
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
