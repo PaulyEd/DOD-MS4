90442525b314425bd107087410b6516d95b96048
PaulyEd <pauledwardswx@gmail.com>
2021-01-12 17:50:21 +0000

    updated add review view to update existing review if user has already reviewed the dev

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'developer',
+        'review_header',
+        'review_body',
+        'review_rating',
+        'review_date',
+        'reviewer',
+    )
+
+admin.site.register(Review, ReviewAdmin)
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Review
-from developers.models import Developer, Language, Framework
+from developers.models import Developer
+from profiles.models import UserProfile
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import ReviewForm
@@ -10,20 +11,40 @@ from .forms import ReviewForm
 def add_review(request, developer_id):
     """ Add a review to a developer """
     developer = get_object_or_404(Developer, pk=developer_id)
-    # if request.method == 'POST':
-    #     form = DeveloperForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         developer = form.save()
-    #         messages.success(request, 'Successfully added developer!')
-    #         return redirect(reverse('developer_detail', args=[developer.id]))
-    #     else:
-    #         messages.error(request, 'Failed to add developer. Please ensure the form is valid.')
-    # else:
-    form = ReviewForm(instance=developer)
+    reviewer = get_object_or_404(UserProfile, pk=request.user.id)
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        review = review_form.save(commit=False)
+        review.developer = developer
+        review.reviewer = reviewer
+        try:
+            review = Review.objects.get(
+                    developer=developer,
+                    reviewer=reviewer,
+                )
+            instance = get_object_or_404(Review, id=review.id)
+            review_form = ReviewForm(request.POST or None, instance=instance)
+            # messages.error(request, 'You have already reviewed this developer')
+            if review_form.is_valid():
+                review_form.save()
+                messages.success(request, f'You have updated your review for {developer.name}')
+                return redirect(reverse('developer_detail', args=[developer.id]))
+            else:
+                messages.error(request, 'Review failed, please ensure the form is valid.')
+        except Review.DoesNotExist:
+            if review_form.is_valid():
+                review_form.save()
+                messages.success(request, f'Thank you for reviewing {developer.name}')
+                return redirect(reverse('developer_detail', args=[developer.id]))
+            else:
+                messages.error(request, 'Review failed, please ensure the form is valid.')
+    else:
+        review_form = ReviewForm(instance=developer)
+        messages.info(request, f'You are reviewing {developer.name}')
 
     template = 'reviews/add_review.html'
     context = {
-        'form': form,
+        'form': review_form,
         'developer': developer,
     }
 
